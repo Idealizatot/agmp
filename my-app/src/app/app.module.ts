56da1f2b9339dcebfe738085f72035d810e615dd
Ryzhenkov <ideal103.5@gmail.com>
2022-02-08 23:39:01 +0300

    add components and interface

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { CourseComponent } from './course/course.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    BreadcrumbsComponent,
+    CourseComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
