639bb38a9d0c542048b6ed07fbd6a86100e0a1bb
Ryzhenkov <ideal103.5@gmail.com>
2022-02-28 23:48:53 +0300

    add first test

--- my-app/src/app/components/course/course.component.spec.ts
+++ my-app/src/app/components/course/course.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseComponent } from './course.component';
+import { Course } from '../../course'
 
 describe('CourseComponent', () => {
   let component: CourseComponent;
@@ -16,14 +17,24 @@ describe('CourseComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CourseComponent);
     component = fixture.componentInstance;
-    component.course.title = '123';
-    fixture.detectChanges();
-    component.course.title = '543';
+    component.course = {
+      id: 1,
+      title: 'a new component',
+      description: '',
+      courseDuration: new Date(),
+      creationDate: new Date(),
+    };
     
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain title', () => {
+    fixture.detectChanges();
+    const titleHTML = fixture.nativeElement as HTMLElement;
+    expect(titleHTML.querySelector('h3')?.textContent).toContain('a new component');
   });
 
 });
